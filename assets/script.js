380ee33b6b6f3642eda78b211062bba690aeddeb
gideonrynn <sarah.c.dillard@gmail.com>
2019-11-19 00:02:53 -0600

    created basic timeblock grid in container

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,13 @@
 
 //define current date with moment.format
-var m = moment().format('dddd, MMMM Do');
+var m = moment().format('dddd, MMMM Do', true);
+
+var mNow = moment();
 
 //added current date to currentDay div
 $('#currentDay').html(m);
+
+
+
 
   
