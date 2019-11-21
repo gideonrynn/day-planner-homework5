3522a1b08c8a5b7a09882290598e6e2ca39d1b53
gideonrynn <sarah.c.dillard@gmail.com>
2019-11-20 21:19:44 -0600

    created for loop to create new table row elements to serve as body for planner

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,32 @@
+$(function () {
 
-//define current date with moment.format for header
+
+//define current date with moment.format for header and add to header
 var m = moment().format('dddd, MMMM Do', true);
-
-//added current date to currentDay div
 $('#currentDay').html(m);
 
-var mNow = moment();
+//define current time for header and add to header
+var mNow = moment().format('h:mm a');
+$('#currentTime').html(mNow);
 
-console.log(mNow);
+//create an array 
 
+//define for loop for adding table to page
+for (var i = 0; i <= 10; i++) {
 
+    var tRow = $("<tr>");
 
+      // Methods run on jQuery selectors return the selector they we run on
+      // This is why we can create and save a reference to a td in the same statement we update its text
+      var setHour = $("<td>").text(1);
+      var setTextArea = $("<td>").text(1);
+      var setbutton = $("<td>").text(1);
+        
+      // Append the newly created table data to the table row
+      tRow.append(setHour, setTextArea, setbutton);
+      // Append the table row to the table body
+      $(".calBody").append(tRow);
 
-
-  
+    };
+    
+});
