d317b64371064ba562d8ed33351150918b8cc711
gideonrynn <sarah.c.dillard@gmail.com>
2019-11-20 22:38:54 -0600

    add 24 hour workday array and button, added saveBtn class to button

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,47 @@
 $(function () {
 
-
 //define current date with moment.format for header and add to header
-var m = moment().format('dddd, MMMM Do', true);
-$('#currentDay').html(m);
+var toDay = moment().format('dddd, MMMM Do', true);
+$('#currentDay').html(toDay);
 
 //define current time for header and add to header
 var mNow = moment().format('h:mm a');
 $('#currentTime').html(mNow);
 
-//create an array 
+//create an array for the hours of the workday
+var workDay = ["9 A.M.", "10 A.M.", "11 A.M.", "12 NOON", "1 P.M.", "2 P.M.", "3 P.M.", "4 P.M.", "5 P.M."]
+var workDay24 = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
+
 
 //define for loop for adding table to page
-for (var i = 0; i <= 10; i++) {
+for (var i = 0; i < 9; i++) {
 
-    var tRow = $("<tr>");
+    //create new row element and add class
+    var newRow = $("<tr>").addClass("time-block");
 
-      // Methods run on jQuery selectors return the selector they we run on
-      // This is why we can create and save a reference to a td in the same statement we update its text
-      var setHour = $("<td>").text(1);
-      var setTextArea = $("<td>").text(1);
-      var setbutton = $("<td>").text(1);
+    //create new elements
+    var newHourCol = $("<td>").attr('id', [i]).text(workDay[i]).addClass("hour");
+    var newTextCol = $("<td>").append("<textarea>");
+    var newButtonCol = $("<td>").append("<button>").addClass("saveBtn");
         
-      // Append the newly created table data to the table row
-      tRow.append(setHour, setTextArea, setbutton);
-      // Append the table row to the table body
-      $(".calBody").append(tRow);
+    // Append the newly created table data to the table row
+    newRow.append(newHourCol, newTextCol, newButtonCol);
+    
+    // Append the table row to the table body
+    $(".calBody").append(newRow);
 
     };
+
+
+    //add display workday on 24 hour clock
+    // btn24Hour.addEventListener("click", function () {
+    //     for (var i = 0; i < 9; i++) {
+
+    //     var newHourCol = $("<td>").text(workDay24[i]).addClass("hour");
+
+    //     newRow.replace(newHourCol);
+    //     }
+    // })
+    //
     
 });
