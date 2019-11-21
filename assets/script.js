105b3a54eb7babd12addedc2dbcf883030a10464
gideonrynn <sarah.c.dillard@gmail.com>
2019-11-20 18:53:46 -0600

    update header and test repo rename

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,14 @@
 
-//define current date with moment.format
+//define current date with moment.format for header
 var m = moment().format('dddd, MMMM Do', true);
-
-var mNow = moment();
 
 //added current date to currentDay div
 $('#currentDay').html(m);
+
+var mNow = moment();
+
+console.log(mNow);
+
 
 
 
