6b7e08dc0ac8655d7acbcbf66df8c464729863e7
gideonrynn <sarah.c.dillard@gmail.com>
2019-11-22 21:31:59 -0600

    set localStorage with user input and time, updated saveBtn border, set local storage to clear previous on save and replace with new list

--- assets/script.js
+++ assets/script.js
@@ -10,15 +10,13 @@ $('#currentTime').html(mNow);
 
 
 //create an array for the hours of the workday, which gets appended to the newly created newHourCol
-var workDay = ["8 A.M.", "9 A.M.", "10", "11", "12 P.M.", "1", "2", "3", "4", "5 P.M."]
+var workDay = ["8 A.M.", "9 A.M.", "10", "11", "12 P.M.", "1", "2", "3", "4", "5 P.M."];
 
 //24 hour array for optional toggle
-var workDay24 = ["08", "09", "10", "11", "12", "13", "14", "15", "16", "17"]
+var workDay24 = ["08", "09", "10", "11", "12", "13", "14", "15", "16", "17"];
 
 //array to be used as a comparison to a moment() variable for determining what colors the rows need to be to represent past, present and future
 var timeComp = ["9:00 am", "10:00 am", "11:00 pm", "12:00 pm", "1:00 pm", "2:00 pm", "3:00 pm", "4:00 pm", "5:00 pm"];
-
-JSON.parse(localStorage.getItem('content'));
 
 
 //define for loop for adding table elements to page
@@ -31,12 +29,14 @@ for (var i = 0; i < workDay.length; i++) {
     var newHourCol = $("<td>").attr('id', [i]).addClass("hour").text(workDay[i]);
     var newTextCol = $("<td>").append("<textarea class='todo'>").attr('id', workDay24[i]);
     var newButtonCol = $("<td>").addClass("saveBtn").append("<button class='saveBtn'>" + "<i class='far fa-address-book'>" + "</i>" + "</button>").attr('id', [i])
+    
         
     // Append the newly created table data to the table row
     newRow.append(newHourCol, newTextCol, newButtonCol);
     
     // Append the table row to the table body
     $(".calBody").append(newRow);
+    $("textarea").attr('id', [i]);
 
     };
 
@@ -64,23 +64,49 @@ for (var i = 0; i < workDay.length; i++) {
       });
 
 
-    //saving to local storage
-    //set variable for the button to be clicked
-    var saveBtnGroup = $(".saveBtn")[0];
-    var toDoClass = $(".todo");
-    console.log(toDoClass);
+    //create var that holds values entered into text area
+    var todaysList = [];
+    console.log(todaysList);
+    // saveBtnGroup.addEventListener("click", addLocalStor);
     
-    saveBtnGroup.addEventListener("click", addLocalStor);
-    
+    $(".saveBtn").on( "click", addNewToDo);
 
-    function addLocalStor () {
-        localStorage.setItem('content', JSON.stringify(toDoClass));
+    //on click, call the addNewToDo function, which processes through each text area, grabs the value, and pushes it to the array
+    function addNewToDo () {
+        todaysList = [];
+
+        $("textarea").each( function() {
+           var t = $(this).val();
+           todaysList.push([t, mNow]);
+           console.log("value of t is " + t);
+        })
+        saveTodos();
+    }
+    
+    function saveTodos () {     
+        var str = JSON.stringify(todaysList);localStorage.setItem('todos', str);
     }
 
-    console.log(localStorage);
+    function getTodos () {
+        
+        //pull all items from local storage to display user appointment inputs at startup
+        var str = localStorage.getItem('todos');
+        todaysList = JSON.parse(str);
+        console.log(todaysList);
+        if (!todaysList) {
+            todaysList = [];
+        }
+    }
 
-    
-    
+    getTodos();
+
+    function listTodos() {
+        var html = "";
+        for (var i in todaysList) {
+            var todo = todaysList[i];
+        }
+    }
+
     //add display workday on 24 hour clock
     // btn24Hour.addEventListener("click", function () {
     //     for (var i = 0; i < 9; i++) {
@@ -92,16 +118,4 @@ for (var i = 0; i < workDay.length; i++) {
     // })
     //
 
-    // //local storage
-    // saveBtn.addEventListener('click', updateOutput);
-
-    // // output_div.textContent = localStorage.getItem('content');
-    // // input_textarea.value = localStorage.getItem('content');
-    
-    // function updateOutput() {
-    //     localStorage.setItem('content', input_textarea.value);
-        
-        // output_div.textContent = input_textarea.value;
-    // }
-    
 });
